67536fb2ae8adf741e5de438864d1093cd5e06a7
Kareem OSAMA <kareem.osama.part@servier.com>
2023-12-20 15:22:39 +0200

    TaskMate

---
+++ src/components/ShowTask.js
@@ -0,0 +1,41 @@
+export const ShowTask = ({ tasklist, setTasklist, task, setTask }) => {
+  const handleDelete = (id) => {
+    const updatedTaskList = tasklist.filter((todo) => todo.id !== id);
+    setTasklist(updatedTaskList);
+  };
+  const handleEdit = (id) => {
+    const selectedTask = tasklist.find((todo) => todo.id === id);
+    setTask(selectedTask);
+  };
+  return (
+    <section className="showTask">
+      <div className="head">
+        <div>
+          <span className="title">Todo</span>
+          <span className="count">{tasklist.length}</span>
+        </div>
+        <button className="clearAll" onClick={() => setTasklist([])}>
+          Clear All
+        </button>
+      </div>
+      <ul>
+        {tasklist.map((task) => (
+          <li key={task.id}>
+            <p>
+              <span className="name">{task.name}</span>
+              <span className="time">{task.time}</span>
+            </p>
+            <i
+              onClick={() => handleEdit(task.id)}
+              className="bi bi-pencil-square"
+            ></i>
+            <i
+              onClick={() => handleDelete(task.id)}
+              className="bi bi-trash"
+            ></i>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
